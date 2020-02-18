ce67ee48236a4df0457fc4664322f5d2c05e5287
Rome Reuter <reuterre9@gmail.com>
2020-02-18 15:19:55 -0600

    wrote two tests

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -9,13 +9,31 @@ namespace LoggingKata.Test
         public void ShouldDoSomething()
         {
             // TODO: Complete Something, if anything
+
+            //Arrange
+            var tacoParser = new TacoParser();
+
+            //Act
+            var actual = tacoParser.Parse("34.073638, -84.677017, Taco Bell Acwort...");
+
+            //Assert
+            Assert.NotNull(actual);
         }
 
         [Theory]
-        [InlineData("Example")]
-        public void ShouldParse(string str)
+        [InlineData("34.324462,-86.503055,Taco Bell Ara...", 34.324462)]
+        public void ShouldParse(string str, double expected)
         {
-            // TODO: Complete Should Parse
+            //Arrange
+            var tacoParserInstance = new TacoParser();
+
+            //Act
+            var actual = tacoParserInstance.Parse(str);
+
+            //Assert
+            Assert.Equal(expected, actual.Location.Latitude);
+            
+            
         }
 
         [Theory]
